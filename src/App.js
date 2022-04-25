b6954d782f7dd7301f06d92123a74690e9f159f5
DeAnne Lombardi <64880045+themockingjedi@users.noreply.github.com>
2022-04-25 19:31:26 -0400

    basic search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header">Hello</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>opensource link etc</footer>
+      </div>
     </div>
   );
 }
