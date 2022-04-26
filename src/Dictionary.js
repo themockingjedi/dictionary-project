e1511e6c326eb9691d91dea4ef63eacf5e28b6a7
DeAnne Lombardi <64880045+themockingjedi@users.noreply.github.com>
2022-04-25 20:39:42 -0400

    api integration, url error after adding axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState(" ");
+  function findDef(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(findDef);
   }
 
   function keywordUpdate(event) {
