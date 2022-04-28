1aa3887df3603af402de432b94a7cfdb12a299e9
DeAnne Lombardi <64880045+themockingjedi@users.noreply.github.com>
2022-04-28 19:42:19 -0400

    meanings work

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,6 +6,8 @@ export default function Meaning(props) {
     return (
       <div className="Meaning">
         <h3>{props.meaning.partOfSpeech}</h3>
+        <p>{props.meaning.definitions[0].definition}</p>
+        <p>{props.meaning.definitions[0].example}</p>
       </div>
     );
   }
--- src/Results.js
+++ src/Results.js
@@ -7,7 +7,7 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        {props.definition.meanings.map(function (meaning, index) {
+        {props.results.meanings.map(function (meaning, index) {
           return (
             <section key={index}>
               <Meaning meaning={meaning} />
